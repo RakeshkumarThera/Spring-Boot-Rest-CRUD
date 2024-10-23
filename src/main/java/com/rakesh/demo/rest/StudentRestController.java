a8a073f61c376eeb3997a9379da7302ceec96327
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-10-23 15:21:28 -0400

    Refactored the code and used @PostConstruct

--- src/main/java/com/rakesh/demo/rest/StudentRestController.java
+++ src/main/java/com/rakesh/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.rakesh.demo.rest;
 
 import com.rakesh.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Rakesh", "Thera"));
+        theStudents.add(new Student("Reshma", "Thera"));
+        theStudents.add(new Student("Ravi", "Thera"));
+    }
 
     //define endpoint for "/students" - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
 
-        theStudents.add(new Student("Rakesh", "Thera"));
-        theStudents.add(new Student("Reshma", "Thera"));
-        theStudents.add(new Student("Ravi", "Thera"));
 
         return theStudents;
     }
